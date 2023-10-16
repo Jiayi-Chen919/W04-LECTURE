9f4729b81f839931f3cb73a7ca4c0a88750dbcce
Jiayi-Chen919 <145276284+Jiayi-Chen919@users.noreply.github.com>
2023-10-16 13:02:34 +0000

    change1016

--- binpacking.py
+++ binpacking.py
@@ -1,4 +1,4 @@
-def first_fit(items, bin_capacity):
+def first_fit(items, bin_capacity, sorting = None):
     '''
     First-fit heuristic for the bin packing problem.
     Takes in a list of items of different sizes, and a bin capacity.
@@ -7,10 +7,26 @@ def first_fit(items, bin_capacity):
     # Initialise a list of bins
     bins = []
 
+    # Additional functionality possibility to sort items first
+    if sorting == None:
+        # no sorting
+        pass
+    elif sorting == 'increasing':
+        #soriting in increasing order
+        items = sorted(items) # need to store the new items
+    elif sorting == 'decreasing':
+        #sorting in decreasing order
+        item = sorted(items, reverse = True) #defult reverse equals false
+    else:
+        raise ValueError('sorting can be either none, decreasing and increasing')
+   # items.sort()
+
+
     # Loop over the list of items
     for i in items:
         # item starts not being placed in a bin
         placed = False
+
         # Loop over the bins (until we find a good one)
         for b in range((len(bins))):
             # Check bin 0; does i fit?
@@ -32,8 +48,9 @@ def first_fit(items, bin_capacity):
 
 
 
-# Testing our function
-items = [2, 1, 3, 2, 1, 2, 3, 1]
-bin_capacity = 4
-bins = first_fit(items, bin_capacity)
-print(bins)  # expected: [4, 4, 4, 3]
+# # Testing our function
+# items = [2, 1, 3, 2, 1, 2, 3, 1]
+# bin_capacity = 4
+# bins = first_fit(items, bin_capacity, sorting = 'decreasing')
+# print(bins)  # expected: [4, 4, 4, 3]
+
